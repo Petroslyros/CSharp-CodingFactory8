f009b4287823049ceb6467deaa2bd72f98c4649f
Petros <petrosluros@gmail.com>
2025-09-09 10:23:23 +0300

    safe read app with tryParse

---
+++ SafeReadApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SafeReadApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal d1 = 0m;
+            decimal d2 = 0m;
+
+            Console.WriteLine("Please input 2 decimals : ");
+
+            if(!decimal.TryParse(Console.ReadLine(), out d1))
+            {
+                Console.WriteLine("Error in input");
+                return;
+            }
+            if (!decimal.TryParse(Console.ReadLine(), out d2))
+            {
+                Console.WriteLine("Error in input");
+                return;
+            }
+
+            Console.WriteLine($"d1 : {d1}, d2: {d2}");
+        }
+    }
+}
---
+++ WriteApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace WriteApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int result = 0;
+
+            //Entoles
+            result = num1 + num2;
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("el-GR");
+
+            //print
+            Console.WriteLine("The sum of {0,6:C2} and {1,6:N2} is {2,10:N2",num1,num2,result);
+            Console.WriteLine($"The sum of {num1,6:N2} + {num2,6:N2} is : {result,10:N2}");
+        }
+    }
+}
