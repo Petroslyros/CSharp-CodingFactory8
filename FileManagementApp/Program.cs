c7b41daf431c6a9fec88574e840fc662b0b76b0d
Petros <petrosluros@gmail.com>
2025-09-18 11:09:00 +0300

    collections, linq, filemanagement

---
+++ FileManagementApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileManagementApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
